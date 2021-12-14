01dd8746969c364e2b62f7751a61b27d176f852d
Panin <panin1970@gmail.com>
2021-12-14 16:59:18 +0300

    First screen

---
+++ WelcomeUser/LoginViewController.swift
@@ -0,0 +1,68 @@
+//
+//  ViewController.swift
+//  WelcomeUser
+//
+//  Created by Александр Панин on 14.12.2021.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var userNameField: UITextField!
+    @IBOutlet weak var userPasswordField: UITextField!
+    
+    private let userNameCorrect = "Alex"
+    private let userPasswodCorrect = "123"
+    
+    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        // Do any additional setup after loading the view.
+//    }
+
+    @IBAction func logInButton() {
+        
+        guard userNameField.text != "" else {
+            showAlert(title: "User Name is Empty", message: "Please enter your User Name", field: userNameField)
+            return
+        }
+        guard userNameField.text == userNameCorrect else {
+            showAlert(title: "User Name is incorrect", message: "Forgot Your User Name?", field: userNameField)
+            return
+        }
+         guard userPasswordField.text != "" else {
+            showAlert(title: "Password is Empty", message: "Please enter your Password", field: userPasswordField)
+             return
+        }
+        guard userPasswordField.text == userPasswodCorrect else {
+            showAlert(title: "Password is incorrect", message: "Forgot Your Password?", field: userPasswordField)
+            return
+        }
+        
+        print("OK")
+    }
+   
+    @IBAction func userNameButton(_ sender: UIButton) {
+        showAlert(title: "Your User Name is", message: userNameCorrect, field: userNameField)
+    }
+    @IBAction func passwordButton(_ sender: UIButton) {
+        showAlert(title: "Your Password is", message: userPasswodCorrect, field: userPasswordField)
+    }
+}
+    
+    // MARK: - Private Methods
+    
+    extension LoginViewController {
+        private func showAlert(title: String, message: String, field: UITextField ) {
+            let alert = UIAlertController (title: title, message: message, preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: .default) {_ in
+                field.text = ""
+            }
+            alert.addAction(okAction)
+            present(alert, animated: true)
+        }
+    }
+    
+
+
